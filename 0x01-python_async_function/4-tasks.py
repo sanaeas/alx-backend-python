6b44ef3eff5a2c5f200374f46f2ee066de7fc283
sanaeas <sanaassim1@gmail.com>
2024-01-15 11:43:13 +0100

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Asynchronous routine task_wait_n"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous routine that spawns task_wait_random n times
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
