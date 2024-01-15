f912c119810401938f1cd0cff441f0e9ec3e7e4a
sanaeas <sanaassim1@gmail.com>
2024-01-15 10:28:48 +0100

    Fix pycodestyle error

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay:int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that waits for a random delay
     between 0 and max_delay seconds
